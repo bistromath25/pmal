3081a3fc471e943dcf9281e8bca1a36c8d29bd44
Brighten Zhang <bistromath25@gmail.com>
2024-12-12 11:58:11 -0500

    auth

---
+++ src/utils/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from './env';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
