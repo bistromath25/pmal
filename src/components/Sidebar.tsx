276177414794111ef7cd5a4cd2edd9bc8e44c245
Brighten Zhang <bistromath25@gmail.com>
2024-12-16 15:07:37 -0500

    resp

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,11 +26,15 @@ export default function Sidebar() {
   const pathname = usePathname();
   return (
     <div className='h-full bg-white border-r-2 p-4'>
-      <div className='flex flex-col gap-2'>
+      <div className='grid grid-cols-2 lg:flex lg:flex-col gap-2'>
         {sidebarOptions.map(({ name, path }) => {
           const isActive = path === pathname;
+          const isSignout = name === 'Sign out';
           return (
-            <div key={`sidebar-option-${name}`}>
+            <div
+              key={`sidebar-option-${name}`}
+              className={`${isSignout ? 'hidden' : ''} lg:block`}
+            >
               <Link href={path}>
                 <div
                   className={`w-full p-2 rounded-lg ${isActive ? 'bg-gray-100' : 'hover:bg-gray-100'} flex flex-row items-center`}
